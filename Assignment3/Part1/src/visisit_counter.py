f1b26a6436ac39d5d7f550797c9ca1e0970154e3
Nhat Anh <91367670+NABlackFox@users.noreply.github.com>
2026-01-14 23:11:07 +0700

    feat: add visit counter class to help with instict reward

---
+++ Assignment3/Part1/src/visisit_counter.py
@@ -0,0 +1,36 @@
+"""
+Visit Counter for Intrinsic Reward Calculation
+"""
+
+from typing import Dict, Tuple
+import math
+
+
+class VisitCounter:
+    def __init__(self, intrinsic_strength: float = 0.1):
+        self.intrinsic_strength = intrinsic_strength
+        self.visit_counts: Dict[Tuple, int] = {}      # (State / Number of Visit)
+
+    def reset_counter(self):
+        """Clear the calculation at the end of each episode"""
+        self.visit_counts.clear()
+
+    def visit(self, state: Tuple) -> int:
+        """
+        Record a visit to a state and return the visit count.
+        """
+        if state not in self.visit_counts:
+            self.visit_counts[state] = 0
+        self.visit_counts[state] += 1
+        return self.visit_counts[state]
+
+    def get_intrinsic_reward(self, state: Tuple) -> float:
+        """
+        Calculate intrinsic reward for visiting a state.
+        Formula: r_i = intrinsic_strength / sqrt(n(s) + 1)
+        """
+        number_of_visit = self.visit_counts.get(state, 0)
+        return self.intrinsic_strength / math.sqrt(number_of_visit + 1)
+
+    def get_visit_count(self, state: Tuple) -> int:
+        return self.visit_counts.get(state, 0)
